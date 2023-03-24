b23aa208a01d2e79d88aa94e2f59b5d360736a34
Dexture <65532415+Ritesh-Hub1999@users.noreply.github.com>
2023-03-24 23:09:54 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {fontFamily: {
+      mullish: [ "ABC Ginto Nord","Noto Sans","Helvetica Neue","Helvetica","Arial","sans-serif","gg sans"
+    ],
+    },
+    },
+  },
+  plugins: [],
+  fontFamily:{
+    whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
